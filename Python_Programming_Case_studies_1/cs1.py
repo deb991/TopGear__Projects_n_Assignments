6e0ba5d224d94d96ee6d6c7ef131fc6cbefeac56
Ozzius <deb991@users.noreply.github.com>
2019-03-01 02:01:44 +0530

    add new lines
    add new lines

--- Python_Programming_Case_studies_1/cs1.py
+++ Python_Programming_Case_studies_1/cs1.py
@@ -31,7 +31,25 @@ def find__dir():
                     with open(output__file, "a") as f:
                         print("The File: {:30s} The Size: {:d} Bytes".format(item, theDictc[item].st_size), os.path.join(path, name), file=f)
 
+                        
+                        ========================================================
+                        
+                        #def file__analizer():
+    #file__IO = input('\nEnter file name here to analize with path:: ')
+    #if os.path.isfile(file__IO):
+    #    print('\nFile exists & initial checking has been completed, So proceeding for the next analizing~~~ :: Flag :: 1')
+
+    #    print('\nBegining initial operation for the content of that file~~~ :: Flag :: 2')
+
+    #   with open(file__IO, 'r') as f:
+    #        for line in f:
+    #            print(f.read())
+    #            line__count = len(f.readline())
+    #            print(line__count)
+    #        f.close()
+
+    #    file__dict = dict()
 
 
-if __name__ == '__main__':
-    find__dir()
+#if __name__ == '__main__':
+#    find__dir()
