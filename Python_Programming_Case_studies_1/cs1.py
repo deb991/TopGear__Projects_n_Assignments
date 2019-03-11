2aac49a5651ce12197d8cf35d4ac6a2db8d9fe62
Ozzius <deb991@users.noreply.github.com>
2019-03-11 21:52:09 +0530

    file updated
    file updated

--- Python_Programming_Case_studies_1/cs1.py
+++ Python_Programming_Case_studies_1/cs1.py
@@ -1,55 +1,25 @@
 #!/usr/bin/env python
 import os
+import string
 import sys
-from datetime import *
-from subprocess import Popen
+import random
+import threading as t
 
-outputFileName = 'Output'
-cmpltName = outputFileName + ".txt"
-output__file = "D:\\My__Env\\PycharmProjects\\TG__lib\\output__data\\Output.txt"
+file__IO = input('\nEnter file name here to analize with path:: ')
 
-def find__dir():
-    srch__path = input('Enter directory path :: ')
+if os.path.isfile(file__IO):
+    print(
+        '\nFile exists & initial checking has been completed, So proceeding for the next analizing~~~ :: Flag :: 1')
+    print('\nBegining initial operation for the content of that file~~~ :: Flag :: 2')
 
+    with open(file__IO, 'r') as f:
+        print(f.readline(), 'Flag :: 3')
+        lineCount = len(f.readlines())
+        data = f.read()
 
-    for path, subdir, files in os.walk(srch__path):
-        for name in srch__path:
-
-            thePath = os.path.join(srch__path)
-            to__thePath = os.chdir(srch__path)
-            theFiles = list(os.listdir(os.getcwd()))
-
-            #for name in files:
-            for something in theFiles:
+        for word in data.split():
+            print('Count__Word ::', len(word), 'Flag :: 4')
 
 
-                theDictc = dict()
-                theStats = os.stat(something)
-                theDictc[something] = theStats
-
-                for item in theDictc:
-                    with open(output__file, "a") as f:
-                        print("The File: {:30s} The Size: {:d} Bytes".format(item, theDictc[item].st_size), os.path.join(path, name), file=f)
-
-                        
-                        ========================================================
-                        
-                        #def file__analizer():
-    #file__IO = input('\nEnter file name here to analize with path:: ')
-    #if os.path.isfile(file__IO):
-    #    print('\nFile exists & initial checking has been completed, So proceeding for the next analizing~~~ :: Flag :: 1')
-
-    #    print('\nBegining initial operation for the content of that file~~~ :: Flag :: 2')
-
-    #   with open(file__IO, 'r') as f:
-    #        for line in f:
-    #            print(f.read())
-    #            line__count = len(f.readline())
-    #            print(line__count)
-    #        f.close()
-
-    #    file__dict = dict()
-
-
-#if __name__ == '__main__':
-#    find__dir()
+    print('\nTotal number of lines :: ', lineCount, 'Flag :: 5')
+    f.close()
