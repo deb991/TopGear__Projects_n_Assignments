5b1ef60cf2827137cb4b584f736980f49f4f7595
Ozzius <deb991@users.noreply.github.com>
2019-03-12 03:28:25 +0530

    File updated
    File updated

--- Python_Programming_Case_studies_1/cs1.py
+++ Python_Programming_Case_studies_1/cs1.py
@@ -1,25 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -i
 import os
 import string
 import sys
 import random
 import threading as t
 
-file__IO = input('\nEnter file name here to analize with path:: ')
+print('This program will process 5 files at a time Max')
 
-if os.path.isfile(file__IO):
-    print(
-        '\nFile exists & initial checking has been completed, So proceeding for the next analizing~~~ :: Flag :: 1')
-    print('\nBegining initial operation for the content of that file~~~ :: Flag :: 2')
+for x in range(1, 5):
 
-    with open(file__IO, 'r') as f:
-        print(f.readline(), 'Flag :: 3')
-        lineCount = len(f.readlines())
-        data = f.read()
+    file__IO = input('\nEnter file name here to analize with path:: ')
 
-        for word in data.split():
-            print('Count__Word ::', len(word), 'Flag :: 4')
+    if os.path.isfile(file__IO):
+        print(
+            '\nFile exists & initial checking has been completed, So proceeding for the next analizing~~~ :: Flag :: 1')
+        print('\nBegining initial operation for the content of that file~~~ :: Flag :: 2')
+
+        with open(file__IO, 'r') as f:
+            data = f.read( )
+            line = data.splitlines( )
+            words = data.split( )
+            spaces = data.split(" ")
+            charc = (len(data) - len(spaces))
+
+            print('\n Line number ::', len(line), '\n Words number ::', len(words), '\n Spaces ::', len(spaces),
+                  '\n Charecters ::', (len(data) - len(spaces)))
+
+        print('EOF')
 
 
-    print('\nTotal number of lines :: ', lineCount, 'Flag :: 5')
-    f.close()
+##To be followed up only command line argument & threading issue for tis program. 
